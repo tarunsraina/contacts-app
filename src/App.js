2280ecef2154f8b0ce1733840928de5a451cbb34
Tarun <tarunsraina483@gmail.com>
2022-04-16 13:00:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import React from "react"
+import  ReactDOM  from 'react-dom';
+import Card from "./components/Card"
+import contacts from './contacts';
+
+function App(){
+  return(
+    <div>
+    <h1 className="heading"> My Contacts </h1>
+    <Card 
+        name={contacts[0].name}
+        img={contacts[0].img}
+        tel={contacts[0].tel}
+        email={contacts[0].email}
+    />
+
+    <Card 
+        name={contacts[1].name}
+        img={contacts[1].img}
+        tel={contacts[1].tel}
+        email={contacts[1].email}
+    />
+
+    <Card 
+        name={contacts[2].name}
+        img={contacts[2].img}
+        tel={contacts[2].tel}
+        email={contacts[2].email}
+    />
     </div>
-  );
+
+
+  )
 }
 
 export default App;
---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+
+
+const contacts =[
+    {
+        name: "beyonce",
+        img:".\beyonce1.jpg",
+        email:"b@beyonce.com",
+        tel : "+746 464525478"
+    },
+    {
+        name: "Dua lipa",
+        img:".\beyonce1.jpg",
+        email:"dua@lipa.com",
+        tel : "+86 49388409"
+    },
+    {
+        name: "shawn",
+        img:".\beyonce1.jpg",
+        email:"shawn@mendes.com",
+        tel : "+76 84736373"
+    }
+]
+
+export default contacts;
